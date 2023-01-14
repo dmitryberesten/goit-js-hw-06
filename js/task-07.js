e68ab205287a9d109a26ea404574d924c0d9dfe8
Dmytrii Beresten <87872240+dmitryberesten@users.noreply.github.com>
2023-01-14 20:06:21 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -3,6 +3,6 @@ const sizeControl = document.querySelector('input#font-size-control')
 const mainText = document.querySelector('span#text')
 
 // Додавання слухача подій
-sizeControl.addEventListener('change', e => {
+sizeControl.addEventListener('input', e => {
     mainText.style.fontSize = `${e.target.value}px` // зміна розміру тексту
 })
